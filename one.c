a8b379a3a6127676459381367fd3416ee1bc1919
PavanChavan <chavanpavan254@gmail.com>
2021-04-29 11:50:21 +0530

    initial commit

---
+++ one.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main(){
+    int number; nguesses=1,guess;
+    srand time(0);
+    number=rand()%100+ 1;
+    printf("The number is %d\n",number);
+   
+    return 0;
+}
+
+
